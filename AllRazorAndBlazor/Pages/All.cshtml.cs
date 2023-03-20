e7ccabc04aab3b349e1ded5f133cad25445bbb0c
David Wengier <git@wengier.com>
2023-03-20 17:48:22 +1100

    Add project files.

---
+++ AllRazorAndBlazor/Pages/All.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AllRazorAndBlazor.Pages
+{
+    public class AllModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AllRazorAndBlazor/Pages/Service.cs
@@ -0,0 +1,19 @@
+﻿namespace AllRazorAndBlazor.Pages
+{
+    public interface IService
+    {
+        string DoSomething<T>();
+        Task<string> DoSomethingAsync();
+
+        IDisposable? GetDisposable();
+    }
+
+    public class Service : IService
+    {
+        public string DoSomething<T>() => typeof(T).ToString();
+
+        public Task<string> DoSomethingAsync() => Task.FromResult("Boo");
+
+        public IDisposable? GetDisposable() => null;
+    }
+}
